07ebcf98d079367262f068f25923f72a694d89a6
karimch-50 <karimchaouki50@gmail.com>
2021-12-24 15:43:03 +0100

    adding a global varaible for styling

--- react-deva-app/src/Component/Card.js
+++ react-deva-app/src/Component/Card.js
@@ -1,28 +1,18 @@
 import React from 'react';
 // import './Card.css';
+import './Card.scss';
 
 const Card = ({title,imageURL,body})=>{
     return(
-        // <div className="card">
-        //     <div className="card-title" style={{"width": "18rem"}}>
-        //         <h3>{title}</h3>
-        //     </div>
-        //     <div className="image-container">
-        //         <img src={imageURL} alt=''/>
-        //     </div>
-        //     <div className="card-body">
-        //         <p>{body}</p>
-        //     </div>
-        // </div>
-    <div className="card shadow p-3 mb-5 bg-body rounded-3" style={{"width": "18rem"}}>
+    <div className="Card">
         <div>
-            <h5 class="card-title text-start fs-4">{title}</h5>
+            <h5 class="Card-title">{title}</h5>
         </div>
         <div>
-            <img className="card-img-top"  src={imageURL} alt="Card image cap"/>
+            <img className="Card-img"  src={imageURL} alt="Cat image"/>
         </div>
-        <div className="card-body">
-            <p className="card-text">{body}</p>
+        <div className="Card-body">
+            <p className="Card-text">{body}</p>
         </div>
     </div>
     );
